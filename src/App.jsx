65e08ef58712c6089703d1bf3b861d1829bec06b
1Kishi <jacobworkgreen@gmail.com>
2025-07-19 20:45:59 +0200

    CSS Foundation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import MainLayout from "./layout/MainLayout";
+import './styles/theme.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Routes>
   );
 }
